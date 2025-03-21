56407c012014cdeb4bc9f787b9504b914dfaa9af
rocketniko <rocketniko@pm.me>
2025-03-21 16:50:33 +0200

    implement a few modes

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,61 @@
 use pyo3::prelude::*;
+use rand::Rng;
 
-#[pyfunction]
-fn pi() -> String {
-    "3.141592653589793238462643383279...".to_string()
-}
 
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
 #[pymodule]
 fn _core(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(pi, m)?)?;
+    m.add_function(wrap_pyfunction!(pi64, m)?)?;
+    m.add_function(wrap_pyfunction!(mc, m)?)?;
+    m.add_function(wrap_pyfunction!(arctan, m)?)?;
     Ok(())
+}
+
+
+/// Builtin f64 value
+#[pyfunction]
+fn pi64() -> String {
+    return std::f64::consts::PI.to_string();
+}
+
+
+/// Naive Monte Carlo
+#[pyfunction]
+fn mc() -> String {
+    let mut rng = rand::rng();
+
+    let mut count: i64 = 0;
+    let mut n: i64 = 0;
+
+    while n <10000000 {
+        n += 1;
+        let x:f64 = rng.random();
+        let y:f64 = rng.random();
+        if x*x + y*y <= 1.0 {
+            count += 1;
+        }
+    }
+
+    let result: f64 = 4.0 * (count as f64)/(n as f64);
+    return result.to_string()
+}
+
+/// Taylor series for arctan(x)
+/// pi = 4 * (1-1/3+1/5-1/7+1/9...)
+#[pyfunction]
+fn arctan() -> String {
+    let mut n: i64 = 0;
+    let mut sum: f64 = 0.0;
+    let mut sign = -1;
+
+    while n<1000000000 {
+        sign = -sign;
+        sum += (sign as f64)/((2 * n + 1) as f64);
+        n += 1;
+    }
+
+    let result: f64 = 4.0 * sum;
+    return result.to_string();
 }
--- src/pismo/__init__.py
+++ src/pismo/__init__.py
@@ -1,5 +1,35 @@
-from pismo._core import pi
 
+from pismo import _core
+
+from pismo._core import *
+
+import argparse
+
+MODES = (
+    'pi64',
+    'mc',
+    'arctan',
+)
 
 def main() -> None:
-    print(pi())
+    parser = argparse.ArgumentParser(
+        description='A collection of algorithms for calculating the number π',
+        epilog="Keep calm and π on!"
+    )
+    g1 = parser.add_argument_group('Floating-point computation')
+    g1.add_argument("--pi64", action="store_true", default=False, help="builtin f64 value")
+    g1.add_argument("--mc", action="store_true", default=False, help="naive Monte Carlo")
+    g1.add_argument("--arctan", action="store_true", default=False, help="Taylor series for arctan")
+
+    args = parser.parse_args()
+
+    func = None
+    for mode in MODES:
+        if getattr(args, mode):
+            func = getattr(_core, mode)
+            break
+
+    if func is not None:
+        print(func())
+    else:
+        parser.print_help()
